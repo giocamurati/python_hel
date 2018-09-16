723a7366e5b588c429785b8df06105150235f0a4
Giovanni Camurati <camurati@eurecom.fr>
2018-09-17 00:04:05 +0200

    python wrapper for hel_lib

---
+++ python_hel/hel.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python2
+
+# Giovanni Camurati
+# camurati@eurecom.fr
+
+import ctypes
+import numpy as np
+lib = ctypes.cdll.LoadLibrary('libhel.so')
+
+def bruteforce(scores, pt1, pt2, ct1, ct2, merge, bins, bit_bound_start,
+        bit_bound_end):
+
+    # conversion to be sure about types
+    scores = np.array(scores).flatten()
+    scores_p = ctypes.c_void_p(scores.ctypes.data)
+
+    pt1 = np.array(pt1, dtype=ctypes.c_ubyte)
+    pt2 = np.array(pt2, dtype=ctypes.c_ubyte)
+    ct1 = np.array(ct1, dtype=ctypes.c_ubyte)
+    ct2 = np.array(ct2, dtype=ctypes.c_ubyte)
+ 
+    merge_value = np.zeros((1), dtype=ctypes.c_int)
+    merge_value = merge
+    nb_bins = np.zeros((1), dtype=ctypes.c_int)
+    nb_bins = bins
+    start = np.zeros((1), dtype=ctypes.c_int)
+    start = bit_bound_start
+    end = np.zeros((1), dtype=ctypes.c_int)
+    end = bit_bound_end
+
+    found = np.zeros((1), dtype=ctypes.c_int)
+    found_key = np.zeros((16), dtype=ctypes.c_ubyte)
+
+    # call to the library function
+    lib.bruteforce(scores_p, pt1.ctypes, pt2.ctypes, ct1.ctypes,
+            ct2.ctypes, found.ctypes, found_key.ctypes, merge_value,
+            nb_bins, start, end)
+
+    return found, found_key
+
+def example():
+    # example mainly taken from ../hel_lib/main_example.cpp
+    from example_data import scores, pt1, pt2, ct1, ct2
+   
+    merge = 2
+    bins = 2048
+    bit_bound_start = 0
+
+    for bit_bound_end in [10, 30]:
+        print ""
+        print "TRYING BRUTEFORCE UP TO RANK 2**%d"%bit_bound_end
+        found, found_key = bruteforce(scores, pt1, pt2, ct1, ct2, merge,
+                bins, bit_bound_start, bit_bound_end)
+
+        if found:
+            print "KEY FOUND!"
+        else:
+            print "KEY NOT FOUND..."
+            print "Let's try with a higher rank"
+
+if __name__ == "__main__":
+    example()
+    
